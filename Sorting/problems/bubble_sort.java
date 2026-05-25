641c8376d96e6ea6a36f1f0dfd16ca88a0ce5604
Atanu Debnath <atanuc2005@gmail.com>
2026-05-25 17:29:51 +0530

    [sorting] bug fix in bubble sort

--- Sorting/problems/bubble_sort.java
+++ Sorting/problems/bubble_sort.java
@@ -6,10 +6,10 @@ public class bubble_sort {
         //bubble sort :
         //1. bubble sort is a process between a pair of elements,
         //it compares two adjasent elements ans swaps them IF fight elements is smaller
-        //2. After iterating through the array and doing the operation inceasing i by 1
-        //and repeat the process
-        for(int i = 1; i < n; i++) {
-            for(int j = i; j < n; j++) {
+        //2. After iterating through the array and doing the operation decrease i by i cause
+        // the greatest value gets placed at ith position after every passing
+        for(int i = n - 1; i > 0; i--) {
+            for(int j = 1; j <= i; j++) {
                 if(nums[j] < nums[j - 1]) {
                     int temp = nums[j];
                     nums[j] = nums[j - 1];
@@ -20,7 +20,7 @@ public class bubble_sort {
         return nums;
     }
     public static void main(String[] args) {
-        int[] nums = {1, 4, 5, 2, 10, 20, 2, 30, 5, 10, 20, 30};
+        int[] nums = {28, 25, 37, 5, 13, 32, 55, 78, 9};
         bubble_sort(nums);
         for(int i = 0; i < nums.length; i++) {
             System.out.print(nums[i] + " ");

e4822b9d95ffc51411ff830b6fa01bc9742e84a8
atanu <atanuc2005@gmail.com>
2026-07-07 17:38:14 +0530

    [array] Q1: find if there exist index i and j such that nums[i] + nums[j] = target; Q2: Find those two indices(it is grunted that there exist such indices)

---
+++ src/Array/problems/TwoSum.java
@@ -0,0 +1,81 @@
+package Array.problems;
+
+import java.util.HashSet;
+
+public class TwoSum {
+    //Question 1: Find if there are 2 elements in the array which sum to target
+    //Brute force
+    //check each pair with nested loop that's it
+
+    //better
+    public static String better(int[] nums, int target) {
+        //We will use hashSet to see if there exist any element which
+        //equals to (target - crr element)
+        HashSet<Integer> set = new HashSet<>();
+        int n = nums.length;
+        for(int num : nums) {
+            if(set.contains(target - num)) {
+                return "Yes";
+            }else {
+                set.add(num);
+            }
+        }
+        return "No";
+    }
+    //optimal
+    //same complexity (O(nlogn)) but without using hashmap
+    public static String optimal(int[] nums, int target) {
+        //at first, we will sort the array in non-decreasing order
+        //we will use two pointer to the start and to the end
+        //if sum of nums[start] and nums[end] is less than target, we simply
+        //more the start pointer to get a greater sum cause if we move end pointer
+        //the sum will get smaller as the array is in ascending order.
+        //vise versa for sum > target
+        int n = nums.length;
+        int start = 0;
+        int end = n - 1;
+        while(start < end) {
+            int sum = nums[start] + nums[end];
+
+            if(sum < target) {
+                start++;
+            }else if(sum > target) {
+                end--;
+            }else {
+                return "Yes";
+            }
+        }
+        return "No";
+    }
+    //Question 2: Find the two indexes where nums[i] + nums[j] = target. (it's granted that these two indices exist)
+    //Same as Q1, just writing the optimal one
+    public static void optimal2(int[] nums, int target) {
+        int n = nums.length;
+        int start = 0;
+        int end = n - 1;
+        int[] ans = new int[2];
+
+        while(start < end) {
+            int sum = nums[start] + nums[end];
+
+            if (sum < target) {
+                start++;
+            } else if (sum > target) {
+                end--;
+            } else {
+                ans[0] = start; ans[1] = end;
+                break;
+            }
+        }
+        System.out.print("Optimal for Q2: " + ans[0] + ",");
+        System.out.println(ans[1]);
+    }
+    public static void main(String[] args) {
+        int[] arr = {2, 6, 5, 8, 11};
+        int target = 17;
+        System.out.println("Better for Q1: " + better(arr, target));
+        System.out.println("Optimal for Q1: " + optimal(arr, target));
+        optimal2(arr, target);
+
+    }
+}
